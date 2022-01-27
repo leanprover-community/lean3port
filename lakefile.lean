376835ea3c4dca48066838bcb1aad5c21995b261
Gabriel Ebner <gebner@gebner.org>
2022-01-27 13:53:02 +0100

    bump

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 open Lake DSL System
 
-def tag : String := "nightly-2022-01-26"
+def tag : String := "nightly-2022-01-27"
 def releaseRepo : String := "leanprover-community/mathport"
 def oleanTarName : String := "lean3-binport.tar.gz"
 def leanTarName : String := "lean3-synport.tar.gz"
@@ -50,6 +50,6 @@ package lean3port (dir) {
   dependencies := #[{
     name := "mathlib",
     -- This git commit here should be the same commit as in the `lakefile.lean` for `mathport`.
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "52a7561299a5a9f7e3ba793a76321d6d40f21aee"
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "23d4e91230a6ffa4ea1258c4b1b4a142bcdcd7c0"
   }]
 }

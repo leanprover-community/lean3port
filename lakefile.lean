674e015668134c78cb743826e10f24f7d8994c56
Mario Carneiro <di.gama@gmail.com>
2024-05-24 00:21:48 +0200

    chore: update lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 open Lake DSL System
 
-def tag : String := "nightly-2024-05-22-08"
+def tag : String := "nightly-2024-05-23-08"
 def releaseRepo : String := "leanprover-community/mathport"
 def oleanTarName : String := "lean3-binport.tar.gz"
 
@@ -20,16 +20,16 @@ def untarReleaseArtifact (repo tag artifact : String) (to : FilePath) : JobM PUn
 package lean3port where
   extraDepTargets := #[`fetchOleans]
 
-target fetchOleans (_pkg) : Unit := do
+target fetchOleans (_pkg) : Unit := Job.async do
   let libDir : FilePath := __dir__ / ".lake" / "build" / "lib"
   IO.FS.createDirAll libDir
   let oldTrace := Hash.ofString tag
-  let _ ← buildFileUnlessUpToDate (libDir / oleanTarName) oldTrace do
+  let _ ← liftM <| buildFileUnlessUpToDate (libDir / oleanTarName) oldTrace do
     logInfo "Fetching oleans for Leanbin"
     untarReleaseArtifact releaseRepo tag oleanTarName libDir
-  return .nil
+  return ((), .nil)
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git"@"1d014c5b8feb62e873167309f0f6a35bf145dc02"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git"@"d8043b0b6f97b54af9ad4f617660c61142476e39"
 
 @[default_target]
 lean_lib Leanbin where
